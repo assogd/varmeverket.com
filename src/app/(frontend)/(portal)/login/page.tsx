05436b64f653fef5ec98a9f0b64a056f4a36dc25
mathiasdag <mathias@asso.gd>
2026-01-22 19:25:33 +0100

    Fix login button blend

--- src/app/(frontend)/(portal)/login/page.tsx
+++ src/app/(frontend)/(portal)/login/page.tsx
@@ -104,8 +104,12 @@ export default async function LoginPage({
                 En inloggningslänk kommer att skickas till dig.
               </p>
             </div>
-            <Button type="submit" className="w-full" variant="primary">
-              Gå vidare
+            <Button
+              type="submit"
+              className="w-full mix-blend-normal"
+              variant="primary"
+            >
+              <span className="mix-blend-multiply text-bg">Gå vidare</span>
             </Button>
           </form>
         )}
